1e04272023da158e07b93a8022db2ca06f672417
Inigoperez <ikbem@plaiaundi.net>
2020-12-02 09:38:53 +0100

    actualizando dependencias

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require("laravel-mix");
-
-mix.js('src/index.js', 'dist')
-   .sass('src/index.scss', 'dist')
-   .setPublicPath('dist');
+mix
+  .sass("src/index.scss", "distMix")
+  .js("src/index.js", "distMix")
+  .sourceMaps()
+  .copy("src/index.html", "distMix")
+  .extract(["leaflet"]);
